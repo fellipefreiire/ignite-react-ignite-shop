824e2371cc37ad63b0b9352f6a10e3655efaf678
Felipe <fellipefreiire3@gmail.com>
2022-09-12 20:21:42 -0300

    carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import Image from 'next/future/image'
+import { Container, Header } from '../styles/pages/app'
+
+globalStyles()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImg}
+          alt=''
+        />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
